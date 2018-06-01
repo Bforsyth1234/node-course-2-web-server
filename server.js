2b6cc3ffdd611c025a3a92bcfecaf4cd1d1096e9
Brooks Forsyth <brooks.forsyth@gmail.com>
2018-05-31 21:41:01 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('vew engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}: ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err)=> {
+        if (err) {
+            console.log('err ', err);
+        }
+    });
+    console.log(log);
+    next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>hello express</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Hi how are u, Im good u, i like the way your feet smell'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Uh oh bad stuff'
+    });
+})
+app.listen(3000);
